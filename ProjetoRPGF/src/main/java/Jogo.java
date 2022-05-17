fd1dcbe0bbb8518384e17667fe0b54791919a83e
bruno.hssouza2 <bruno.hssouza2@CAS555563C136.senacsp.edu.br>
2022-05-16 21:44:42 -0300

    Criação e Interação do jogo, batalhas.

---
+++ ProjetoRPGF/src/main/java/Jogo.java
@@ -0,0 +1,45 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author bruno.hssouza2
+ */
+public class Jogo {
+    public static void main(String[] args) {
+        String choice_Goption;
+        Scanner sc = new Scanner(System.in);
+        
+        do {
+            System.out.println("--------------------------------------");
+            System.out.println("BEM VINDO!");
+            System.out.println("Essa voz, a voz que você escuta e trouxe você até aqui, somos nós,\n os assolados flagelos que tivemos nossas vidas roubadas pelo demônio.");
+            System.out.println("A esta altura você deve saber a fonte do seu poder, estamos com você concedendo o que nos resta de energia vital para acabar com este pesadelo.");
+            System.out.println("Agora que adentramos dentro do covil, estaremos com você afim de cumprir nossa profecia. Existem salas neste covil,\n sabemos pois somos intimamente ligados com o demônio, assim como somos com você, herói.");
+            System.out.println("Existem 2 salas iniciais, onde encontram-se personificações do demônio");
+            System.out.println("Por favor, escolha de acordo com o seu instinto.");
+            System.out.println("1) Sala da esquerda");
+            System.out.println("2) Sala da direita");
+            choice_Goption = sc.nextLine();
+            
+            switch(choice_Goption) {
+                case "1":
+                    
+                case "2":
+                    
+            }
+            
+            
+            
+        } while ( );
+                
+        
+        
+    }
+    
+}
